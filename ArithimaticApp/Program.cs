f5434352bd7a2c788f4e6051244a07cb62837318
George Raphael <georgie817@gmail.com>
2024-05-10 10:18:21 +0300

    Condition statements

--- ArithimaticApp/Program.cs
+++ ArithimaticApp/Program.cs
@@ -4,20 +4,31 @@ int num1 = 0;
 decimal num2 = 0;
 decimal sum = 0;
 
-Console.Write("Enter first number: ");
-num1 = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Enter first number: ");
+//num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Enter second number: ");
-num2 = Convert.ToDecimal(Console.ReadLine());
+//Console.Write("Enter second number: ");
+//num2 = Convert.ToDecimal(Console.ReadLine());
 
-sum = num1 + num2;
+//sum = num1 + num2;
 
 //The compiler will infer the type based on te value.
-var age = 10;
+//var age = 10;
 //Assignment
 // += -= *= %= /= =
 
+//Ternary Operator
+int umri = 17;
+string message = umri >= 18 ? "Adult" : "Child";
+Console.Write(message);
 
+//Variable Scope
+
+if(umri == 17)
+{
+    string ujumbe = "karibu";
+}
+//Console.WriteLine(ujumbe); ujumbe cant be accessed because it is out of the scope of its declaration
 
 
 
