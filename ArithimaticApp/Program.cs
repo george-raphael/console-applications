785d822f8c5eaf98d365ead2442d1fbc561d0f47
George Raphael <georgie817@gmail.com>
2024-05-10 10:08:13 +0300

    Arithimatic, Logical & Assignment Operators

---
+++ ArithimaticApp/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("******* Welcome *******");
+
+int num1 = 0;
+decimal num2 = 0;
+decimal sum = 0;
+
+Console.Write("Enter first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+num2 = Convert.ToDecimal(Console.ReadLine());
+
+sum = num1 + num2;
+
+//The compiler will infer the type based on te value.
+var age = 10;
+//Assignment
+// += -= *= %= /= =
+
+
+
+
+
+
+
+
