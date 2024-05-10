bcb1f15a3510e64a50b3d26ff069c1bb5d9f7a2f
George Raphael <georgie817@gmail.com>
2024-05-10 09:06:22 +0300

    Add project files.

---
+++ DisplayOutput/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+string jina = "Karissa";
+
+/*
+Console.WriteLine(jina);
+Console.WriteLine("Jina langu ni: " + jina);//concatination
+Console.WriteLine($"Jina langu ni: {jina}");//Interpolation
+Console.WriteLine("Jina langu ni: {0}", jina);//Formatted String
+*/
+
+int umri = 30;
+Console.WriteLine(umri);
+Console.WriteLine("Nina miaka: " + umri);
---
+++ ReadInput/Program.cs
@@ -0,0 +1,14 @@
+﻿
+string name = "Frank";
+float age = 10.0f;
+
+Console.WriteLine("What is your name?");
+name = Console.ReadLine();
+
+Console.WriteLine("How old are you?");
+string userInput = Console.ReadLine();
+
+age =  float.Parse(userInput);
+
+Console.WriteLine("Your name is: " + name);
+Console.WriteLine($"You are {age} years old");
