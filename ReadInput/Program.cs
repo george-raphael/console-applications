a21233bfec3b3257870f964a4f14bdc05df54640
George Raphael <georgie817@gmail.com>
2024-05-10 09:54:21 +0300

    Debugging and troubleshooting

--- ReadInput/Program.cs
+++ ReadInput/Program.cs
@@ -1,14 +1,28 @@
 ﻿
-string name = "Queen";
-float age = 10.0f;
+string? firstName = "Queen";
+string lastName = string.Empty;
+int age;
+char gender  = char.MinValue;
+float salary;
 
-Console.WriteLine("What is your name?");
-name = Console.ReadLine();
+Console.WriteLine("What is your first name?");
+firstName = Console.ReadLine();
+
+Console.WriteLine("What is your last name?");
+lastName = Console.ReadLine();
+
+Console.WriteLine("What is your gender?");
+gender = Convert.ToChar(Console.ReadLine());
+
+Console.WriteLine("What is your salary?");
+salary = float.Parse(Console.ReadLine());
+
 
 Console.WriteLine("How old are you?");
 string userInput = Console.ReadLine();
 
-age =  float.Parse(userInput);
+age =  int.Parse(userInput);
 
-Console.WriteLine("Your name is: " + name);
+Console.WriteLine($"Your name is: {firstName} {lastName}");
 Console.WriteLine($"You are {age} years old");
+Console.WriteLine($"You are: {gender}");
