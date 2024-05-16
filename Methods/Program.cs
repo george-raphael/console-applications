2c5af0121782e3677f2e266ae99a53ae0eb79b97
George Raphael <georgie817@gmail.com>
2024-05-16 11:40:04 +0300

    Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,50 @@
+﻿
+Console.WriteLine("******Methods******");
+
+//Anatomy - (syntax)
+
+//void functions/methods
+void GetInfo(string name)
+{
+    Console.WriteLine($"My name is {name}");
+}
+
+GetInfo("Hamisi");
+//functions with return statements
+string GetUserInfo(string name)
+{
+    return $"My name is {name}";
+}
+
+Console.WriteLine(GetUserInfo("Anna"));
+
+//methods with optional arguments
+string GetUserData(string name = "Bill")
+{
+    return $"My name is {name}";
+}
+
+Console.WriteLine(GetUserData());
+
+//methods with nullable arguments
+int GetMyUserData(string? name)
+{
+    //return $"My name is {name}";
+    if (!String.IsNullOrEmpty(name))
+    {
+        return name.Length;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+//methods with nullable arguments
+string SetUserName(string? name)
+{
+    name ??= "Daudi";
+
+    return name;
+}
+Console.WriteLine(SetUserName("Dotto"));
